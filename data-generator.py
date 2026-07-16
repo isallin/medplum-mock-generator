fecdb2d23d6d78bb97e446a985537f7d5588c8fa
isabellin01 <isabel.lin@sptech.school>
2026-07-16 13:39:15 -0300

    fix: generate random elements

--- data-generator.py
+++ data-generator.py
@@ -1,113 +1,121 @@
 import os
 import requests
 from faker import Faker
+from urllib import response
 from dotenv import load_dotenv
-from faker.providers import DynamicProvider
 
 load_dotenv()
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-base_url = os.getenv('BASE_URL')
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+base_url = os.getenv("BASE_URL")
 
-## SPECIAL CONDITIONS FOR GENERATING DATA AND PROVIDERS
-gender = ['male', 'female', 'other', 'unknown']
-gender_probability = [0.45, 0.45, 0.05, 0.05]
-gender_provider = DynamicProvider(
-    provider_name="gender",
-    elements=gender,
-    weights=gender_probability
-)
-
-active = [True, False]
-active_probability = [0.85, 0.15]
-active_provider = DynamicProvider(
-    provider_name="active",
-    elements=active,
-    weights=active_probability
-)
-
-telecom = ['phone', 'email']
-telecom_provider = DynamicProvider(
-    provider_name="telecom",
-    elements=telecom,
-    weights=[0.7, 0.3]
-)
-
-fake = Faker('pt_BR')
-fake.add_provider(gender_provider)
-fake.add_provider(active_provider)
-fake.add_provider(telecom_provider)
+fake = Faker("pt_BR")
 
 def generate_telecom():
-    telecom_type = fake.telecom()
-    if telecom_type == 'phone':
-        return {
-            "system": "phone",
-            "value": fake.phone_number(),
-            "use": "mobile"
-        }
+    telecom_type = fake.random_element(elements=["phone", "email"])
+    if telecom_type == "phone":
+        return {"system": "phone", "value": fake.phone_number(), "use": "mobile"}
     else:
-        return {
-            "system": "email",
-            "value": fake.email(),
-            "use": "home"
-        }
+        return {"system": "email", "value": fake.email(), "use": "home"}
 
-def generate_patient_fhire(): 
-  patient =  {
-    "resourceType": "Patient",
-    "name": [
-      {
-        "use": "official",
-        "given": [fake.first_name(), fake.middle_name()],
-        "family": fake.last_name()
-      }
-    ],
-    "gender": fake.random_element(gender),
-    "birthDate": fake.date_of_birth(minimum_age=1, maximum_age=100).strftime("%d-%m-%Y"),
-    "active": fake.active(),
-    "telecom": [
-      generate_telecom()
-    ],
-    "address": [
-      {
-        "line": [fake.street_address()],
-        "city": "São Paulo",
-        "state": "SP",
-        "postalCode": "01000-000"
-      }
-    ]
-  }
-  return patient
+def generate_patient() -> dict:
+    selected_gender = fake.random_element(
+        elements=["male", "female"]
+    )
+    if selected_gender == "male":
+        first_name = fake.first_name_male()
+    elif selected_gender == "female":
+        first_name = fake.first_name_female()
+    else:
+        first_name = fake.first_name()
+
+    return {
+        "resourceType": "Patient",
+        "gender": selected_gender,
+        "name": [
+            {
+                "use": "official",
+                "given": [first_name],
+                "family": fake.last_name(),
+            }
+        ],
+        "birthDate": fake.date_of_birth(minimum_age=1, maximum_age=100).isoformat(),
+        "active": fake.random_element(elements=[True, False]),
+        "telecom": [generate_telecom()],
+        "address": [
+            {
+                "use": "home",
+                "line": [fake.street_address()],
+                "city": fake.city(),
+                "state": fake.estado_sigla(),
+                "postalCode": fake.postcode(),
+                "country": "BR",
+            }
+        ],
+        "identifier": [
+            {
+                "use": "official",
+                "system": "http://saude.goias.gov/cpf",
+                "value": fake.cpf(),
+            }
+        ],
+    }
 
 def authentication_medplum():
     token_url = f"{base_url}/oauth2/token"
     data = {
-            'grant_type': 'client_credentials',
-            'client_id': client_id,
-            'client_secret': client_secret
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
     }
-    response = requests.post(
-        token_url,
-        data=data
-    )
+    response = requests.post(token_url, data=data)
     return response.json()["access_token"]
 
-def generate_fake_data(token):
-    patient = generate_patient_fhire()
-    return patient
+def batch_bundle(size=100) -> dict:
+    entries = []
+    for _ in range(size):
+        patient_data = generate_patient()
+        entries.append({
+            "request": {
+                "method": "POST",
+                "url": "Patient"
+            },
+            "resource": patient_data
+        })
+        
+    return {
+        "resourceType": "Bundle",
+        "type": "batch",
+        "entry": entries
+    }
+
+def send_batch_data(token, bundle):
+    response = requests.post(
+        f"{base_url}/fhir/R4/Patient",
+        json=bundle,
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Content-Type": "application/fhir+json",
+            "accept": "application/fhir+json",
+        },
+        timeout=10,
+    )
+    response.raise_for_status()
+    return response.json()
+
 
 def main():
-    try:
-        print("Authenticating in medplum...")
-        token = authentication_medplum()
-        print("Authentication completed successfully!")
-        
-        print("Generating fake data...")
-        fake_data = generate_fake_data(token)
-        print("Fake data generated successfully!")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    for i in range(2):
+        print(f"Generating patient {i + 1}º...")
+        try:
+            token = authentication_medplum()
+            bundle = batch_bundle(size=100)
+            data = send_batch_data(token, bundle)
+            print(f"Patient {i + 1}º generated successfully!")
+            print(f"ID: {data.get('id')}")
+            print(f"lastUpdated: {data.get('meta', {}).get('lastUpdated')}")
+        except Exception as e:
+            print(f"An error occurred while generating patient {i + 1}º: {e}")
 
 if __name__ == "__main__":
     main()
